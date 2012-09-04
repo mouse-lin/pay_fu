aa61d6d98f9bbd5c39a8338eb87a6958607d125a
mouseshi <linhongshimouse@gmail.com>
2012-09-04 10:42:53 +0800

    add tenpay config to config template & ignore null config

--- lib/pay_fu/engine.rb
+++ lib/pay_fu/engine.rb
@@ -9,18 +9,33 @@ module PayFu
         require 'activemerchant_patch_for_china'
 
         PayFu::CONFIGS = YAML.load_file(config_file)[Rails.env]
+
+        # config for paypal
         paypal_config = PayFu::CONFIGS["paypal"]
+        unless paypal_config.hash_values?("")
+          ActiveMerchant::Billing::Base.mode = paypal_config["mode"].to_sym
+          ActiveMerchant::Billing::PaypalGateway.new(
+            :login => paypal_config["login"],
+            :password => paypal_config["password"],
+            :signature => paypal_config["signature"]
+          )
+        end
 
-        ActiveMerchant::Billing::Base.mode = paypal_config["mode"].to_sym
-        ActiveMerchant::Billing::PaypalGateway.new(
-          :login => paypal_config["login"],
-          :password => paypal_config["password"],
-          :signature => paypal_config["signature"]
-        )
+        # config for alipay
+        alipay_config = PayFu::CONFIGS["alipay"]
+        unless alipay_config.has_values?("")
+          ActiveMerchant::Billing::Integrations::Alipay::KEY = alipay_config["key"]
+          ActiveMerchant::Billing::Integrations::Alipay::ACCOUNT = alipay_config["account"]
+          ActiveMerchant::Billing::Integrations::Alipay::EMAIL = alipay_config["email"]
+        end
 
-        ActiveMerchant::Billing::Integrations::Alipay::KEY = PayFu::CONFIGS["alipay"]["key"]
-        ActiveMerchant::Billing::Integrations::Alipay::ACCOUNT = PayFu::CONFIGS["alipay"]["account"]
-        ActiveMerchant::Billing::Integrations::Alipay::EMAIL = PayFu::CONFIGS["alipay"]["email"]
+        # config for tenpay
+        tenpay_config = PayFu::CONFIGS["tenpay"]
+        unless tenpay_config.hash_values("")
+          ActiveMerchant::Billing::Integrations::Alipay::SPID = PayFu::CONFIGS["tenpay"]["spid"]
+          ActiveMerchant::Billing::Integrations::Alipay::KEY = PayFu::CONFIGS["tenpay"]["key"]
+        end
+
       end
     end
   end
